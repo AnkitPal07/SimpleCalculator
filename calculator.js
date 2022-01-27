f9f4da650ef78b1ed5445c6d12790a771f84bdb8
AnkitPal07 <65612200+AnkitPal07@users.noreply.github.com>
2022-01-28 03:10:46 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,27 +1,26 @@
-divide
+
 function divide() {
     var num1 = document.getElementById("firstNumber").value;
     var num2 = document.getElementById("secondNumber").value;
     document.getElementById("result").value = Number(num1) / Number(num2);
+}
 
-multiply
 function multiply() {
     var num1 = document.getElementById("firstNumber").value;
     var num2 = document.getElementById("secondNumber").value;
     document.getElementById("result").value = Number(num1) * Number(num2);
 
-add
+}
 
 function add() {
     var num1 = document.getElementById("firstNumber").value;
     var num2 = document.getElementById("secondNumber").value;
     document.getElementById("result").value = Number(num1) + Number(num2);
-
+}
 function subtract() {
     var num1 = document.getElementById("firstNumber").value;
     var num2 = document.getElementById("secondNumber").value;
     document.getElementById("result").value = Number(num1) - Number(num2);
-master
-master
-master
+
 }
+
